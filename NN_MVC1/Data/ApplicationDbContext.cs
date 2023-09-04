9fe39b826da63ceac81f671ca33bd91ee4b2f0c5
Nuno Nogueira <ghosy21@gmail.com>
2023-09-05 00:13:31 +0100

    first commit

---
+++ NN_MVC1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NN_MVC1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
